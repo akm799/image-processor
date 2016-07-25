0f42ea55dd8a1bb9fd39ef5dd59eae5eb379a621
Thanos Mavroidis <thanosmavroidis@Unknown-b8-8d-12-3b-57-b8.home>
2016-07-25 22:46:26 +0100

    Added unit tests

--- src/test/java/com/test/image/hough/HoughTransformTest.java
+++ src/test/java/com/test/image/hough/HoughTransformTest.java
@@ -14,21 +14,13 @@ public class HoughTransformTest {
     private final int height = 10;
     private GrayScaleImage image = new GrayScaleImage(width, height);
 
-    private final HoughTransform underTest = new BasicHoughTransform(10);
-
-    @Before
-    public void setUp() {
-        for (int j=0 ; j<height ; j++) {
-            for (int i=0 ; i<width ; i++) {
-                if (i == j) {
-                    image.setPixel(i, j, Constants.MAX_INTENSITY);
-                }
-            }
-        }
-    }
+    private HoughTransform underTest;
 
     @Test
-    public void shouldPerformHoughTransform() {
+    public void shouldFindNegativeObliqueLine() {
+        setUpNegativeObliqueLine();
+        underTest = new BasicHoughTransform(10);
+
         final Histogram2D histogram = underTest.transform(image);
         Assert.assertNotNull(histogram);
         final Bin2D[] bins = histogram.getBins();
@@ -48,6 +40,54 @@ public class HoughTransformTest {
         final int[] indexes = {0, 11, 22, 33, 44, 55, 66, 77, 88, 99};
         for (int i=0 ; i<size ; i++) {
             Assert.assertEquals(indexes[i], entries.get(i));
+        }
+    }
+
+    private void setUpNegativeObliqueLine() {
+        for (int j=0 ; j<height ; j++) {
+            for (int i=0 ; i<width ; i++) {
+                if (i == j) {
+                    image.setPixel(i, j, Constants.MAX_INTENSITY);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void shouldFindPositiveObliqueLine() {
+        setUpPositiveObliqueLine();
+        underTest = new BasicHoughTransform(2);
+
+        final Histogram2D histogram = underTest.transform(image);
+        Assert.assertNotNull(histogram);
+        final Bin2D[] bins = histogram.getBins();
+        Assert.assertNotNull(bins);
+        Assert.assertEquals(1, bins.length);
+
+        final double x = 3*Math.PI/4;
+        final double y = 0;
+        final int size = 10;
+        Assert.assertEquals(x, bins[0].x(), 0.0);
+        Assert.assertEquals(y, bins[0].y(), 0.001);
+
+        final IntList entries = bins[0].entries();
+        Assert.assertNotNull(entries);
+        Assert.assertEquals(size, entries.size());
+
+        final int[] indexes = {9, 18, 27, 36, 45, 54, 63, 72, 81, 90};
+        for (int i=0 ; i<size ; i++) {
+            Assert.assertEquals(indexes[i], entries.get(i));
+        }
+    }
+
+    private void setUpPositiveObliqueLine() {
+        final int maxJ = height - 1;
+        for (int j=0 ; j<height ; j++) {
+            for (int i=0 ; i<width ; i++) {
+                if (i == j) {
+                    image.setPixel(i, maxJ - j, Constants.MAX_INTENSITY);
+                }
+            }
         }
     }
 }
