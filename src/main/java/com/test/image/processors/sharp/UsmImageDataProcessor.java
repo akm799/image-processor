ee01e356e62dc5088bd2cacf82c3b1980ac00e68
Thanos Mavroidis <thanosmavroidis@Thanoss-MBP.home>
2019-03-08 19:51:30 +0000

    Minor change.

--- src/main/java/com/test/image/processors/sharp/UsmImageDataProcessor.java
+++ src/main/java/com/test/image/processors/sharp/UsmImageDataProcessor.java
@@ -12,11 +12,13 @@ import com.test.image.processors.scale.IntensityScaleImageDataProcessor;
  *
  * https://en.wikipedia.org/wiki/Unsharp_masking
  *
+ * https://homepages.inf.ed.ac.uk/rbf/HIPR2/unsharp.htm
+ * 
  * Created by Thanos Mavroidis on 06/03/2019.
  */
 final class UsmImageDataProcessor implements ImageDataProcessor {
-    private final ImageDataProcessor blur = new FilterImageDataProcessor(new GaussianImageFilter(1, 3.0f));
-    private final IntensityScaleImageDataProcessor scale = new IntensityScaleImageDataProcessor(0.5f);
+    private final ImageDataProcessor blur = new FilterImageDataProcessor(new GaussianImageFilter(5, 9.0f));
+    private final IntensityScaleImageDataProcessor scale = new IntensityScaleImageDataProcessor(0.7f);
     private final CombinationImageDataProcessor add = new AddImageDataProcessor();
     private final CombinationImageDataProcessor subtract = new SubtractImageDataProcessor();
 
