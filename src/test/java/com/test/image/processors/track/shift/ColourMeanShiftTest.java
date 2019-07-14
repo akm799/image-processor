aa91088b040ccf7391f4ad6eeb5334eb3ff6aada
Thanos Mavroidis <thanosmavroidis@Thanoss-MBP.home>
2019-07-14 21:04:39 +0100

    Added more unit tests.

--- src/test/java/com/test/image/processors/track/shift/ColourMeanShiftTest.java
+++ src/test/java/com/test/image/processors/track/shift/ColourMeanShiftTest.java
@@ -19,56 +19,116 @@ public class ColourMeanShiftTest {
     private final int halfWidth = width/2;
     private final int halfHeight = height/2;
 
-    private final int firstQuadrantIndex = 0;
-    private final int secondQuadrantIndex = 1;
-    private final int thirdQuadrantIndex = 2;
-    private final int fourthQuadrantIndex = 3;
+    private final int topLeftQuadrant = 0;
+    private final int topRightQuadrantIndex = 1;
+    private final int bottomLeftQuadrantIndex = 2;
+    private final int bottomRightQuadrantIndex = 3;
+
+    private final int red = ColourHelper.getRgb(255, 0, 0);
+    private final int green = ColourHelper.getRgb(0, 255, 0);
+    private final int blue = ColourHelper.getRgb(0, 0, 255);
+    private final int white = ColourHelper.getRgb(255, 255, 255);
 
     @Test
-    public void shouldShiftTowardsMean() {
-        final ColourCubeHistogram initialWindow = defineInitialWindow(); // Target window is all red.
-        final ColourCubeHistogram shiftedWindow = defineShiftedWindow(); // Window has a red bottom right quadrant and different colours everywhere else.
+    public void shouldShiftLeft() {
+        final int initialColour = red;
+        final int[] quadrantColours = {initialColour, green, initialColour, blue};
+        final int xShift = -3;
+        final int yShift = 0;
+        testShiftTowardsMean(initialColour, quadrantColours, xShift, yShift);
+    }
+
+    @Test
+    public void shouldShiftRight() {
+        final int initialColour = red;
+        final int[] quadrantColours = {green, initialColour, blue, initialColour};
+        final int xShift = 2;
+        final int yShift = 0;
+        testShiftTowardsMean(initialColour, quadrantColours, xShift, yShift);
+    }
+
+    @Test
+    public void shouldShiftUp() {
+        final int initialColour = red;
+        final int[] quadrantColours = {initialColour, initialColour, green, blue};
+        final int xShift = 0;
+        final int yShift = -3;
+        testShiftTowardsMean(initialColour, quadrantColours, xShift, yShift);
+    }
+
+    @Test
+    public void shouldShiftDown() {
+        final int initialColour = red;
+        final int[] quadrantColours = {green, blue, initialColour, initialColour};
+        final int xShift = 0;
+        final int yShift = 2;
+        testShiftTowardsMean(initialColour, quadrantColours, xShift, yShift);
+    }
+
+    @Test
+    public void shouldShiftTowardsTopLeftQuadrant() {
+        final int initialColour = red;
+        final int[] quadrantColours = {initialColour, white, green, blue};
+        final int xShift = -3;
+        final int yShift = -3;
+        testShiftTowardsMean(initialColour, quadrantColours, xShift, yShift);
+    }
+
+    @Test
+    public void shouldShiftTowardsTopRightQuadrant() {
+        final int initialColour = red;
+        final int[] quadrantColours = {white, initialColour, green, blue};
+        final int xShift = 2;
+        final int yShift = -3;
+        testShiftTowardsMean(initialColour, quadrantColours, xShift, yShift);
+    }
+
+    @Test
+    public void shouldShiftTowardsBottomLeftQuadrant() {
+        final int initialColour = red;
+        final int[] quadrantColours = {white, green, initialColour, blue};
+        final int xShift = -3;
+        final int yShift = 2;
+        testShiftTowardsMean(initialColour, quadrantColours, xShift, yShift);
+    }
+
+    @Test
+    public void shouldShiftTowardsBottomRightQuadrant() {
+        final int initialColour = red;
+        final int[] quadrantColours = {white, green, blue, initialColour};
+        final int xShift = 2;
+        final int yShift = 2;
+        testShiftTowardsMean(initialColour, quadrantColours, xShift, yShift);
+    }
+
+    private void testShiftTowardsMean(int initialColour, int [] quadrantColours, int xShift, int yShift) {
+        final ColourCubeHistogram initialWindow = defineInitialWindow(initialColour);
+        final ColourCubeHistogram shiftedWindow = defineShiftedWindow(quadrantColours);
         final ColourCubeDifference comparison = new ColourCubeDifferenceImpl(initialWindow, shiftedWindow);
 
         // Shift from the shifted window back towards the initial window.
         final Point shift = ColourMeanShift.shift(shiftedWindow, comparison);
         Assert.assertNotNull(shift);
-        Assert.assertEquals(2, shift.x); // New centre is at the centre of the bottom right quadrant, i.e. a positive x-shit.
-        Assert.assertEquals(2, shift.y); // New centre is at the centre of the bottom right quadrant, i.e. a positive y-shit.
+        Assert.assertEquals(xShift, shift.x);
+        Assert.assertEquals(yShift, shift.y);
     }
 
-    private ColourCubeHistogram defineInitialWindow() {
+    private ColourCubeHistogram defineInitialWindow(int rgb) {
         final MutableColourCubeHistogram window = new MutableColourCubeHistogramImpl(width, height, nDivsInSide);
         for (int i=0 ; i<width*height ; i++) {
-            addRed(i, window);
+            add(rgb, i, window);
         }
 
         return window;
     }
 
-    private ColourCubeHistogram defineShiftedWindow() {
+    private ColourCubeHistogram defineShiftedWindow(int [] quadrantColours) {
         final MutableColourCubeHistogram window = new MutableColourCubeHistogramImpl(width, height, nDivsInSide);
 
         for (int i=0 ; i<width*height ; i++) {
-            switch (findQuadrantIndex(i)) {
-                case firstQuadrantIndex:
-                    addWite(i, window);
-                    break;
-
-                case secondQuadrantIndex:
-                    addGreen(i, window);
-                    break;
-
-                case thirdQuadrantIndex:
-                    addBlue(i, window);
-                    break;
-
-                case fourthQuadrantIndex:
-                    addRed(i, window);
-                    break;
-
-                default: throw new IllegalArgumentException("Illegal quadrant index: " + findQuadrantIndex(i));
-            }
+            final int quadrantIndex = findQuadrantIndex(i);
+            final int colour = quadrantColours[quadrantIndex];
+            add(colour, i, window);
         }
 
         return window;
@@ -79,35 +139,19 @@ public class ColourMeanShiftTest {
         final int qy = (pixelIndex/width)/halfHeight;
 
         if (qx == 0 && qy == 0) {
-            return firstQuadrantIndex;
+            return topLeftQuadrant;
         } else if (qx == 1 && qy == 0) {
-            return secondQuadrantIndex;
+            return topRightQuadrantIndex;
         } else if (qx == 0 && qy == 1) {
-            return thirdQuadrantIndex;
+            return bottomLeftQuadrantIndex;
         } else if (qx == 1 && qy == 1) {
-            return fourthQuadrantIndex;
+            return bottomRightQuadrantIndex;
         } else {
             throw new IllegalArgumentException("Illegal pixel index: " + pixelIndex);
         }
     }
-
-    private void addRed(int pixelIndex, MutableColourCubeHistogram histogram) {
-        add(255, 0, 0, pixelIndex, histogram);
-    }
-
-    private void addGreen(int pixelIndex, MutableColourCubeHistogram histogram) {
-        add(0, 255, 0, pixelIndex, histogram);
-    }
-
-    private void addBlue(int pixelIndex, MutableColourCubeHistogram histogram) {
-        add(0, 0, 255, pixelIndex, histogram);
-    }
-
-    private void addWite(int pixelIndex, MutableColourCubeHistogram histogram) {
-        add(255, 255, 255, pixelIndex, histogram);
-    }
-
-    private void add(int r, int g, int b, int pixelIndex, MutableColourCubeHistogram histogram) {
-        histogram.add(pixelIndex, ColourHelper.getRgb(r, g, b));
+    
+    private void add(int rgb, int pixelIndex, MutableColourCubeHistogram histogram) {
+        histogram.add(pixelIndex, rgb);
     }
 }
