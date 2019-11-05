6070fb22e8bc2aa98a11c1aa4ab546eaf59da9ba
Thanos Mavroidis <thanosmavroidis@Thanoss-MBP.home>
2019-11-05 20:29:43 +0000

    Optimized the window image processor.

--- src/main/java/com/test/image/processors/window/WindowImageProcessor.java
+++ src/main/java/com/test/image/processors/window/WindowImageProcessor.java
@@ -4,6 +4,8 @@ import com.test.image.AbstractFileImageProcessor;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -38,53 +40,40 @@ public final class WindowImageProcessor extends AbstractFileImageProcessor {
     @Override
     public BufferedImage processImage(BufferedImage image) {
         checkAllWindowsAreWithinTheImage(image);
-        return drawWBufferedImageWithWindows(image, windows);
+        return drawBufferedImageWithWindows(image, windows);
     }
 
-    private BufferedImage drawWBufferedImageWithWindows(BufferedImage input, Collection<ColouredWindow> windows) {
-        final int width = input.getWidth();
-        final int height = input.getHeight();
-        final BufferedImage output = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        for (int j=0 ; j<height ; j++) {
-            for (int i=0 ; i<width ; i++) {
-                final int rgb = getOutputPixelColour(input, windows, i, j);
-                output.setRGB(i, j, rgb);
-            }
+    private BufferedImage drawBufferedImageWithWindows(BufferedImage input, Collection<ColouredWindow> windows) {
+        final BufferedImage output = deepCopy(input);
+        for (ColouredWindow window : windows) {
+            drawBufferedImageWithWindows(output, window);
         }
 
         return output;
     }
 
-    private int getOutputPixelColour(BufferedImage input, Collection<ColouredWindow> windows, int x, int y) {
-        final ColouredWindow borderPixelWindow = isWindowBorderPixel(windows, x, y);
-        if (borderPixelWindow == null) {
-            return input.getRGB(x, y);
-        } else {
-            return borderPixelWindow.rgb;
-        }
+    // https://stackoverflow.com/questions/3514158/how-do-you-clone-a-bufferedimage
+    private BufferedImage deepCopy(BufferedImage source) {
+        final ColorModel colourModel = source.getColorModel();
+        final boolean isAlphaPremultiplied = colourModel.isAlphaPremultiplied();
+        final WritableRaster raster = source.copyData(null);
+
+        return new BufferedImage(colourModel, raster, isAlphaPremultiplied, null);
     }
 
-    private ColouredWindow isWindowBorderPixel(Collection<ColouredWindow> windows, int x, int y) {
-        for (ColouredWindow window : windows) {
-            if (isWindowBorderPixel(window, x, y)) {
-                return window;
-            }
+    private void drawBufferedImageWithWindows(BufferedImage output, ColouredWindow window) {
+        final int rgb = window.rgb;
+
+        // Horizontal window lines
+        for (int i=window.xMin ; i<=window.xMax ; i++) {
+            output.setRGB(i, window.yMin, rgb);
+            output.setRGB(i, window.yMax, rgb);
         }
 
-        return null;
-    }
-
-    private boolean isWindowBorderPixel(Window window, int x, int y) {
-        if (x == window.xMin && (window.yMin <= y && y <= window.yMax)) {
-            return true;
-        } else if (x == window.xMax && (window.yMin <= y && y <= window.yMax)) {
-            return true;
-        } else if (y == window.yMin && (window.xMin <= x && x <= window.xMax)) {
-            return true;
-        } else if (y == window.yMax && (window.xMin <= x && x <= window.xMax)) {
-            return true;
-        } else {
-            return false;
+        // Vertical window lines
+        for (int j=window.yMin ; j<=window.yMax; j++) {
+            output.setRGB(window.xMin, j, rgb);
+            output.setRGB(window.xMax, j, rgb);
         }
     }
 
